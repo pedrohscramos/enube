0edc9eb45648a87b42040117b90705ac6a139712
Pedro Henrique Ramos <pedrohscramos@gmail.com>
2024-06-28 17:39:02 -0300

    Login e autenticação de usuários

--- internal/infra/database/interfaces.go
+++ internal/infra/database/interfaces.go
@@ -6,3 +6,8 @@ type UserInterface interface {
 	Create(user *entity.User) error
 	FindByEmail(email string) (*entity.User, error)
 }
+
+type SupplierInterface interface {
+	FindAll(page, limit int, sort string) ([]entity.Supplier, error)
+	FindByID(id int) (*entity.Supplier, error)
+}
